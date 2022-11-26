29a8eeec197274b72b61d6f8d73dbae062ed596b
Shunjie Wan <64866870+Eason710085@users.noreply.github.com>
2022-11-25 22:27:05 -0500

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,18 +8,18 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxb69e95bc68e749d3',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '30ff2aacefe9d453b9de79ce36549168',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '重庆',
+  CITY: '重庆',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '乖乖',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -42,9 +42,9 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-07-12' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        
       ],
     },
   ],
