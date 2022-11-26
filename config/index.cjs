ed6e650f5505d3222efb6170292326fe24a231f6
Shunjie Wan <64866870+Eason710085@users.noreply.github.com>
2022-11-25 23:11:22 -0500

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,23 +21,23 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '乖乖',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'gh_e0700ea17b38',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'rQ4G1C2FTwsCnUAqJSFxBbjacbhFl0Xaj9g4KUe3Lk8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '04-11',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '生日', name: '小赵', year: '2004', date: '04-11',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '一力', year: '2001', date: '06-11',
         },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
+//         {
+//           type: '节日', name: '我们在一起已经', year: '2022', date: '07-12',
+//         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
@@ -45,19 +45,19 @@ const USER_CONFIG = {
         { keyword: 'love_day', date: '2022-07-12' },
         // 结婚纪念日
         
-      ],
+      //],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'nj5YQbF3DccjRZdStMEIhIyh3wI9gmu_IEKeLt8eKtE	',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'gh_e0700ea17b38',
     }
   ],
 
